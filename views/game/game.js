d16a88f6805597b080e5ba8bb3b4d99567c0d8f0
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-05 16:13:47 +0200

    Added for work in progress.

---
+++ views/game/game.js
@@ -0,0 +1,12 @@
+import { Base } from "../base/base.js";
+import { game} from "/entities/models/game.js";
+
+export class Game extends Base {
+
+  init() {
+    super.init();
+  }
+
+  hanldeKeyboardEvent(event) {
+  }
+}
