d8aea88ce732c0a268fd2fe696056781790c6c8e
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-07 15:33:15 +0200

    Added.

---
+++ infrastructure/landscapesMapping/landscapeWeapons.js
@@ -0,0 +1,33 @@
+import { Landscape } from "../constants/Landscapes.js";
+
+export const landscapeWeapons = [
+    { landscape: Landscape.Lappland, path: "/views/resources/images/svg/weapons/Lappland.svg" },
+    { landscape: Landscape.Norrbotten, path: "/views/resources/images/svg/weapons/Norrbotten.svg" },
+    { landscape: Västerbotten, path: "/views/resources/images/svg/weapons/Västerbotten.svg" },
+    { landscape: Jämland, path: "/views/resources/images/svg/weapons/Jämland.svg" },
+    { landscape: Ångermanland, path: "/views/resources/images/svg/weapons/Ångermanland.svg" },
+    { landscape: Härjedalen, path: "/views/resources/images/svg/weapons/Härejedalen.svg" },
+    { landscape: Medelpad, path: "/views/resources/images/svg/weapons/Medelpad.svg" },
+    { landscape: Hälsingland, path: "/views/resources/images/svg/weapons/Hälsingland.svg" },
+    { landscape: Gästrikland, path: "/views/resources/images/svg/weapons/Gästrikland.svg" },
+
+    // Svealand
+    { landscape: Dalarna, path: "/views/resources/images/svg/weapons/Dalarna.svg" },
+    { landscape: Värmland, path: "/views/resources/images/svg/weapons/Värmland.svg" },
+    { landscape: Västmanland, path: "/views/resources/images/svg/weapons/Västmanland.svg" },
+    { landscape: Uppland, path: "/views/resources/images/svg/weapons/Uppland.svg" },
+    { landscape: Närke, path: "/views/resources/images/svg/weapons/Närke.svg" },
+    { landscape: Södermanland, path: "/views/resources/images/svg/weapons/Södermanland.svg" },
+
+    // Götaland
+    { landscape: Dalsland, path: "/views/resources/images/svg/weapons/Dalsland.svg" },
+    { landscape: Bohuslän, path: "/views/resources/images/svg/weapons/Bohuslän.svg" },
+    { landscape: Västergötland, path: "/views/resources/images/svg/weapons/Västergötland.svg" },
+    { landscape: Östergötland, path: "/views/resources/images/svg/weapons/Östergötland.svg" },
+    { landscape: Halland, path: "/views/resources/images/svg/weapons/Halland.svg" },
+    { landscape: Småland, path: "/views/resources/images/svg/weapons/Småland.svg" },
+    { landscape: Gotland, path: "/views/resources/images/svg/weapons/Gotland.svg" },
+    { landscape: Öland, path: "/views/resources/images/svg/weapons/Öland.svg" },
+    { landscape: Blekinge, path: "/views/resources/images/svg/weapons/Blekinge.svg" },
+    { landscape: Skåne, path: "/views/resources/images/svg/weapons/Skåne.svg" }
+];
