c474883ac608cc34b8ea86429a1c6d9548be7ff9
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-07 09:46:57 +0200

    Renamed.

---
+++ entities/constants/landscapes.js
@@ -0,0 +1,32 @@
+const LandscapeEnum = Object.freeze({
+    // Norrland
+    Lappland: "Lappland",
+    Norrbotten: "Norrbotten",
+    Västerbotten: "Västerbotten",
+    Jämland: "Jämland",
+    Ångermanland: "Ångermanland",
+    Härjedalen: "Härjedalen",
+    Medelpad: "Medelpad",
+    Hälsingland: "Hälsingland",
+    Gästrikland: "Gästrikland",
+
+    // Svealand
+    Dalarna: "Dalarna",
+    Värmland: "Värmland",
+    Västmanland: "Västmanland",
+    Uppland: "Uppland",
+    Närke: "Närke",
+    Södermanland: "Södermanland",
+
+    // Götaland
+    Dalsland: "Dalsland",
+    Bohuslän: "Bohuslän",
+    Västergötland: "Västergötland",
+    Östergötland: "Östergötland",
+    Halland: "Halland",
+    Småland: "Småland",
+    Gotland: "Gotland",
+    Öland: "Öland",
+    Blekinge: "Blekinge",
+    Skåne: "Skåne",
+});
