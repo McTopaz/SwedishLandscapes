2069a6857cf10c56e16553a4422a07900ad653f4
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-07 15:33:00 +0200

    Added.

---
+++ infrastructure/categorySvg.js
@@ -0,0 +1,8 @@
+import { CategoryTypes } from "/entities/constants/categoryTypes.js";
+
+export const CategorySvg = {
+    [CategoryTypes.Weapon]: "/views/resources/images/svg/Shield.svg",
+    [CategoryTypes.Flowers]: "/views/resources/images/svg/Flower.svg",
+    [CategoryTypes.Fishes]: "/views/resources/images/svg/Fish.svg",
+    [CategoryTypes.ChecmicalElements]: "/views/resources/images/svg/ChemicalElement.svg",
+};
