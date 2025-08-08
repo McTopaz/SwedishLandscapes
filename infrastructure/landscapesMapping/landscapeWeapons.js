caa8ffb6223b57f9a1c670f0686a19cbfb14dfa4
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-08 10:45:12 +0200

    Removed any Å, Ä, Ö in file names which caused problem displaying the the SVGs of the landscapes weapons.

--- infrastructure/landscapesMapping/landscapeWeapons.js
+++ infrastructure/landscapesMapping/landscapeWeapons.js
@@ -3,31 +3,31 @@ import { Landscapes } from "/entities/constants/Landscapes.js";
 export const landscapeWeapons = [
     { landscape: Landscapes.Lappland, path: "/views/resources/images/svg/weapons/Lappland.svg" },
     { landscape: Landscapes.Norrbotten, path: "/views/resources/images/svg/weapons/Norrbotten.svg" },
-    { landscape: Landscapes.Västerbotten, path: "/views/resources/images/svg/weapons/Västerbotten.svg" },
-    { landscape: Landscapes.Jämland, path: "/views/resources/images/svg/weapons/Jämland.svg" },
-    { landscape: Landscapes.Ångermanland, path: "/views/resources/images/svg/weapons/Ångermanland.svg" },
-    { landscape: Landscapes.Härjedalen, path: "/views/resources/images/svg/weapons/Härejedalen.svg" },
+    { landscape: Landscapes.Västerbotten, path: "/views/resources/images/svg/weapons/Vasterbotten.svg" },
+    { landscape: Landscapes.Jämland, path: "/views/resources/images/svg/weapons/Jamland.svg" },
+    { landscape: Landscapes.Ångermanland, path: "/views/resources/images/svg/weapons/Angermanland.svg" },
+    { landscape: Landscapes.Härjedalen, path: "/views/resources/images/svg/weapons/Harejedalen.svg" },
     { landscape: Landscapes.Medelpad, path: "/views/resources/images/svg/weapons/Medelpad.svg" },
-    { landscape: Landscapes.Hälsingland, path: "/views/resources/images/svg/weapons/Hälsingland.svg" },
-    { landscape: Landscapes.Gästrikland, path: "/views/resources/images/svg/weapons/Gästrikland.svg" },
+    { landscape: Landscapes.Hälsingland, path: "/views/resources/images/svg/weapons/Halsingland.svg" },
+    { landscape: Landscapes.Gästrikland, path: "/views/resources/images/svg/weapons/Gastrikland.svg" },
 
     // Svealand
     { landscape: Landscapes.Dalarna, path: "/views/resources/images/svg/weapons/Dalarna.svg" },
-    { landscape: Landscapes.Värmland, path: "/views/resources/images/svg/weapons/Värmland.svg" },
-    { landscape: Landscapes.Västmanland, path: "/views/resources/images/svg/weapons/Västmanland.svg" },
+    { landscape: Landscapes.Värmland, path: "/views/resources/images/svg/weapons/Varmland.svg" },
+    { landscape: Landscapes.Västmanland, path: "/views/resources/images/svg/weapons/Vastmanland.svg" },
     { landscape: Landscapes.Uppland, path: "/views/resources/images/svg/weapons/Uppland.svg" },
-    { landscape: Landscapes.Närke, path: "/views/resources/images/svg/weapons/Närke.svg" },
-    { landscape: Landscapes.Södermanland, path: "/views/resources/images/svg/weapons/Södermanland.svg" },
+    { landscape: Landscapes.Närke, path: "/views/resources/images/svg/weapons/Narke.svg" },
+    { landscape: Landscapes.Södermanland, path: "/views/resources/images/svg/weapons/Sodermanland.svg" },
 
     // Götaland
     { landscape: Landscapes.Dalsland, path: "/views/resources/images/svg/weapons/Dalsland.svg" },
-    { landscape: Landscapes.Bohuslän, path: "/views/resources/images/svg/weapons/Bohuslän.svg" },
-    { landscape: Landscapes.Västergötland, path: "/views/resources/images/svg/weapons/Västergötland.svg" },
-    { landscape: Landscapes.Östergötland, path: "/views/resources/images/svg/weapons/Östergötland.svg" },
+    { landscape: Landscapes.Bohuslän, path: "/views/resources/images/svg/weapons/Bohuslan.svg" },
+    { landscape: Landscapes.Västergötland, path: "/views/resources/images/svg/weapons/Vastergotland.svg" },
+    { landscape: Landscapes.Östergötland, path: "/views/resources/images/svg/weapons/Ostergotland.svg" },
     { landscape: Landscapes.Halland, path: "/views/resources/images/svg/weapons/Halland.svg" },
-    { landscape: Landscapes.Småland, path: "/views/resources/images/svg/weapons/Småland.svg" },
+    { landscape: Landscapes.Småland, path: "/views/resources/images/svg/weapons/Smaland.svg" },
     { landscape: Landscapes.Gotland, path: "/views/resources/images/svg/weapons/Gotland.svg" },
-    { landscape: Landscapes.Öland, path: "/views/resources/images/svg/weapons/Öland.svg" },
+    { landscape: Landscapes.Öland, path: "/views/resources/images/svg/weapons/Oland.svg" },
     { landscape: Landscapes.Blekinge, path: "/views/resources/images/svg/weapons/Blekinge.svg" },
-    { landscape: Landscapes.Skåne, path: "/views/resources/images/svg/weapons/Skåne.svg" }
+    { landscape: Landscapes.Skåne, path: "/views/resources/images/svg/weapons/Skane.svg" }
 ];
