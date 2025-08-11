fa39a7e43304135d56328512b05b8ce3237a9a89
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-11 08:48:26 +0200

    Renamed SVG-folder from "weapon" to "provincialCoatOfArms".

--- infrastructure/landscapesMapping/provincialCoatOfArms.js
+++ infrastructure/landscapesMapping/provincialCoatOfArms.js
@@ -1,33 +1,33 @@
 import { Landscapes } from "/entities/constants/Landscapes.js";
 
 export const provincialCoatOfArms = [
-    { landscape: Landscapes.Lappland, path: "/views/resources/images/svg/weapons/Lappland.svg" },
-    { landscape: Landscapes.Norrbotten, path: "/views/resources/images/svg/weapons/Norrbotten.svg" },
-    { landscape: Landscapes.Västerbotten, path: "/views/resources/images/svg/weapons/Vasterbotten.svg" },
-    { landscape: Landscapes.Jämtland, path: "/views/resources/images/svg/weapons/Jamtland.svg" },
-    { landscape: Landscapes.Ångermanland, path: "/views/resources/images/svg/weapons/Angermanland.svg" },
-    { landscape: Landscapes.Härjedalen, path: "/views/resources/images/svg/weapons/Harjedalen.svg" },
-    { landscape: Landscapes.Medelpad, path: "/views/resources/images/svg/weapons/Medelpad.svg" },
-    { landscape: Landscapes.Hälsingland, path: "/views/resources/images/svg/weapons/Halsingland.svg" },
-    { landscape: Landscapes.Gästrikland, path: "/views/resources/images/svg/weapons/Gastrikland.svg" },
+    { landscape: Landscapes.Lappland, path: "/views/resources/images/svg/provincialCoatOfArms/Lappland.svg" },
+    { landscape: Landscapes.Norrbotten, path: "/views/resources/images/svg/provincialCoatOfArms/Norrbotten.svg" },
+    { landscape: Landscapes.Västerbotten, path: "/views/resources/images/svg/provincialCoatOfArms/Vasterbotten.svg" },
+    { landscape: Landscapes.Jämtland, path: "/views/resources/images/svg/provincialCoatOfArms/Jamtland.svg" },
+    { landscape: Landscapes.Ångermanland, path: "/views/resources/images/svg/provincialCoatOfArms/Angermanland.svg" },
+    { landscape: Landscapes.Härjedalen, path: "/views/resources/images/svg/provincialCoatOfArms/Harjedalen.svg" },
+    { landscape: Landscapes.Medelpad, path: "/views/resources/images/svg/provincialCoatOfArms/Medelpad.svg" },
+    { landscape: Landscapes.Hälsingland, path: "/views/resources/images/svg/provincialCoatOfArms/Halsingland.svg" },
+    { landscape: Landscapes.Gästrikland, path: "/views/resources/images/svg/provincialCoatOfArms/Gastrikland.svg" },
 
     // Svealand
-    { landscape: Landscapes.Dalarna, path: "/views/resources/images/svg/weapons/Dalarna.svg" },
-    { landscape: Landscapes.Värmland, path: "/views/resources/images/svg/weapons/Varmland.svg" },
-    { landscape: Landscapes.Västmanland, path: "/views/resources/images/svg/weapons/Vastmanland.svg" },
-    { landscape: Landscapes.Uppland, path: "/views/resources/images/svg/weapons/Uppland.svg" },
-    { landscape: Landscapes.Närke, path: "/views/resources/images/svg/weapons/Narke.svg" },
-    { landscape: Landscapes.Södermanland, path: "/views/resources/images/svg/weapons/Sodermanland.svg" },
+    { landscape: Landscapes.Dalarna, path: "/views/resources/images/svg/provincialCoatOfArms/Dalarna.svg" },
+    { landscape: Landscapes.Värmland, path: "/views/resources/images/svg/provincialCoatOfArms/Varmland.svg" },
+    { landscape: Landscapes.Västmanland, path: "/views/resources/images/svg/provincialCoatOfArms/Vastmanland.svg" },
+    { landscape: Landscapes.Uppland, path: "/views/resources/images/svg/provincialCoatOfArms/Uppland.svg" },
+    { landscape: Landscapes.Närke, path: "/views/resources/images/svg/provincialCoatOfArms/Narke.svg" },
+    { landscape: Landscapes.Södermanland, path: "/views/resources/images/svg/provincialCoatOfArms/Sodermanland.svg" },
 
     // Götaland
-    { landscape: Landscapes.Dalsland, path: "/views/resources/images/svg/weapons/Dalsland.svg" },
-    { landscape: Landscapes.Bohuslän, path: "/views/resources/images/svg/weapons/Bohuslan.svg" },
-    { landscape: Landscapes.Västergötland, path: "/views/resources/images/svg/weapons/Vastergotland.svg" },
-    { landscape: Landscapes.Östergötland, path: "/views/resources/images/svg/weapons/Ostergotland.svg" },
-    { landscape: Landscapes.Halland, path: "/views/resources/images/svg/weapons/Halland.svg" },
-    { landscape: Landscapes.Småland, path: "/views/resources/images/svg/weapons/Smaland.svg" },
-    { landscape: Landscapes.Gotland, path: "/views/resources/images/svg/weapons/Gotland.svg" },
-    { landscape: Landscapes.Öland, path: "/views/resources/images/svg/weapons/Oland.svg" },
-    { landscape: Landscapes.Blekinge, path: "/views/resources/images/svg/weapons/Blekinge.svg" },
-    { landscape: Landscapes.Skåne, path: "/views/resources/images/svg/weapons/Skane.svg" }
+    { landscape: Landscapes.Dalsland, path: "/views/resources/images/svg/provincialCoatOfArms/Dalsland.svg" },
+    { landscape: Landscapes.Bohuslän, path: "/views/resources/images/svg/provincialCoatOfArms/Bohuslan.svg" },
+    { landscape: Landscapes.Västergötland, path: "/views/resources/images/svg/provincialCoatOfArms/Vastergotland.svg" },
+    { landscape: Landscapes.Östergötland, path: "/views/resources/images/svg/provincialCoatOfArms/Ostergotland.svg" },
+    { landscape: Landscapes.Halland, path: "/views/resources/images/svg/provincialCoatOfArms/Halland.svg" },
+    { landscape: Landscapes.Småland, path: "/views/resources/images/svg/provincialCoatOfArms/Smaland.svg" },
+    { landscape: Landscapes.Gotland, path: "/views/resources/images/svg/provincialCoatOfArms/Gotland.svg" },
+    { landscape: Landscapes.Öland, path: "/views/resources/images/svg/provincialCoatOfArms/Oland.svg" },
+    { landscape: Landscapes.Blekinge, path: "/views/resources/images/svg/provincialCoatOfArms/Blekinge.svg" },
+    { landscape: Landscapes.Skåne, path: "/views/resources/images/svg/provincialCoatOfArms/Skane.svg" }
 ];
