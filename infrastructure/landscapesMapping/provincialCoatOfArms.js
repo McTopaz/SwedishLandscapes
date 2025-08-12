5936152de5d250b70ede8aa1a286634ea4fb5f6e
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-12 11:35:46 +0200

    Refactored to return a list of Landscape.js elements.

--- infrastructure/landscapesMapping/provincialCoatOfArms.js
+++ infrastructure/landscapesMapping/provincialCoatOfArms.js
@@ -1,33 +1,35 @@
 import { Landscapes } from "/entities/constants/Landscapes.js";
+import { Landscape } from "/entities/models/Landscape.js";
 
-export const provincialCoatOfArms = [
-    { landscape: Landscapes.Lappland, path: "/views/resources/images/svg/provincialCoatOfArms/Lappland.svg" },
-    { landscape: Landscapes.Norrbotten, path: "/views/resources/images/svg/provincialCoatOfArms/Norrbotten.svg" },
-    { landscape: Landscapes.Västerbotten, path: "/views/resources/images/svg/provincialCoatOfArms/Vasterbotten.svg" },
-    { landscape: Landscapes.Jämtland, path: "/views/resources/images/svg/provincialCoatOfArms/Jamtland.svg" },
-    { landscape: Landscapes.Ångermanland, path: "/views/resources/images/svg/provincialCoatOfArms/Angermanland.svg" },
-    { landscape: Landscapes.Härjedalen, path: "/views/resources/images/svg/provincialCoatOfArms/Harjedalen.svg" },
-    { landscape: Landscapes.Medelpad, path: "/views/resources/images/svg/provincialCoatOfArms/Medelpad.svg" },
-    { landscape: Landscapes.Hälsingland, path: "/views/resources/images/svg/provincialCoatOfArms/Halsingland.svg" },
-    { landscape: Landscapes.Gästrikland, path: "/views/resources/images/svg/provincialCoatOfArms/Gastrikland.svg" },
+export const provincialCoatOfArms = Object.freeze([
+    // Norrland
+    new Landscape(Landscapes.Lappland, "/views/resources/images/svg/provincialCoatOfArms/Lappland.svg"),
+    new Landscape(Landscapes.Norrbotten, "/views/resources/images/svg/provincialCoatOfArms/Norrbotten.svg"),
+    new Landscape(Landscapes.Västerbotten, "/views/resources/images/svg/provincialCoatOfArms/Vasterbotten.svg"),
+    new Landscape(Landscapes.Jämtland, "/views/resources/images/svg/provincialCoatOfArms/Jamtland.svg"),
+    new Landscape(Landscapes.Ångermanland, "/views/resources/images/svg/provincialCoatOfArms/Angermanland.svg"),
+    new Landscape(Landscapes.Härjedalen, "/views/resources/images/svg/provincialCoatOfArms/Harjedalen.svg"),
+    new Landscape(Landscapes.Medelpad, "/views/resources/images/svg/provincialCoatOfArms/Medelpad.svg"),
+    new Landscape(Landscapes.Hälsingland, "/views/resources/images/svg/provincialCoatOfArms/Halsingland.svg"),
+    new Landscape(Landscapes.Gästrikland, "/views/resources/images/svg/provincialCoatOfArms/Gastrikland.svg"),
 
     // Svealand
-    { landscape: Landscapes.Dalarna, path: "/views/resources/images/svg/provincialCoatOfArms/Dalarna.svg" },
-    { landscape: Landscapes.Värmland, path: "/views/resources/images/svg/provincialCoatOfArms/Varmland.svg" },
-    { landscape: Landscapes.Västmanland, path: "/views/resources/images/svg/provincialCoatOfArms/Vastmanland.svg" },
-    { landscape: Landscapes.Uppland, path: "/views/resources/images/svg/provincialCoatOfArms/Uppland.svg" },
-    { landscape: Landscapes.Närke, path: "/views/resources/images/svg/provincialCoatOfArms/Narke.svg" },
-    { landscape: Landscapes.Södermanland, path: "/views/resources/images/svg/provincialCoatOfArms/Sodermanland.svg" },
+    new Landscape(Landscapes.Dalarna, "/views/resources/images/svg/provincialCoatOfArms/Dalarna.svg"),
+    new Landscape(Landscapes.Värmland, "/views/resources/images/svg/provincialCoatOfArms/Varmland.svg"),
+    new Landscape(Landscapes.Västmanland, "/views/resources/images/svg/provincialCoatOfArms/Vastmanland.svg"),
+    new Landscape(Landscapes.Uppland, "/views/resources/images/svg/provincialCoatOfArms/Uppland.svg"),
+    new Landscape(Landscapes.Närke, "/views/resources/images/svg/provincialCoatOfArms/Narke.svg"),
+    new Landscape(Landscapes.Södermanland, "/views/resources/images/svg/provincialCoatOfArms/Sodermanland.svg"),
 
     // Götaland
-    { landscape: Landscapes.Dalsland, path: "/views/resources/images/svg/provincialCoatOfArms/Dalsland.svg" },
-    { landscape: Landscapes.Bohuslän, path: "/views/resources/images/svg/provincialCoatOfArms/Bohuslan.svg" },
-    { landscape: Landscapes.Västergötland, path: "/views/resources/images/svg/provincialCoatOfArms/Vastergotland.svg" },
-    { landscape: Landscapes.Östergötland, path: "/views/resources/images/svg/provincialCoatOfArms/Ostergotland.svg" },
-    { landscape: Landscapes.Halland, path: "/views/resources/images/svg/provincialCoatOfArms/Halland.svg" },
-    { landscape: Landscapes.Småland, path: "/views/resources/images/svg/provincialCoatOfArms/Smaland.svg" },
-    { landscape: Landscapes.Gotland, path: "/views/resources/images/svg/provincialCoatOfArms/Gotland.svg" },
-    { landscape: Landscapes.Öland, path: "/views/resources/images/svg/provincialCoatOfArms/Oland.svg" },
-    { landscape: Landscapes.Blekinge, path: "/views/resources/images/svg/provincialCoatOfArms/Blekinge.svg" },
-    { landscape: Landscapes.Skåne, path: "/views/resources/images/svg/provincialCoatOfArms/Skane.svg" }
-];
+    new Landscape(Landscapes.Dalsland, "/views/resources/images/svg/provincialCoatOfArms/Dalsland.svg"),
+    new Landscape(Landscapes.Bohuslän, "/views/resources/images/svg/provincialCoatOfArms/Bohuslan.svg"),
+    new Landscape(Landscapes.Västergötland, "/views/resources/images/svg/provincialCoatOfArms/Vastergotland.svg"),
+    new Landscape(Landscapes.Östergötland, "/views/resources/images/svg/provincialCoatOfArms/Ostergotland.svg"),
+    new Landscape(Landscapes.Halland, "/views/resources/images/svg/provincialCoatOfArms/Halland.svg"),
+    new Landscape(Landscapes.Småland, "/views/resources/images/svg/provincialCoatOfArms/Smaland.svg"),
+    new Landscape(Landscapes.Gotland, "/views/resources/images/svg/provincialCoatOfArms/Gotland.svg"),
+    new Landscape(Landscapes.Öland, "/views/resources/images/svg/provincialCoatOfArms/Oland.svg"),
+    new Landscape(Landscapes.Blekinge, "/views/resources/images/svg/provincialCoatOfArms/Blekinge.svg"),
+    new Landscape(Landscapes.Skåne, "/views/resources/images/svg/provincialCoatOfArms/Skane.svg")
+]);
