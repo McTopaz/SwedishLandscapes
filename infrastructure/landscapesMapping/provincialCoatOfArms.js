921ad365526573ea572958c4d2b7c5003bce4494
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-13 13:24:04 +0200

    Made absolute paths into relative paths of imports.

--- infrastructure/landscapesMapping/provincialCoatOfArms.js
+++ infrastructure/landscapesMapping/provincialCoatOfArms.js
@@ -1,5 +1,5 @@
-import { Landscapes } from "/entities/constants/Landscapes.js";
-import { Landscape } from "/entities/models/Landscape.js";
+import { Landscapes } from "../../entities/constants/Landscapes.js";
+import { Landscape } from "../../entities/models/Landscape.js";
 
 export const provincialCoatOfArms = Object.freeze([
     // Norrland
