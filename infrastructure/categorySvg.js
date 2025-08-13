13bf5a7c81ac57f895b35b541f27b9e8317093b7
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-13 11:13:10 +0200

    Added BASE_PATH for paths to SVG-images.

--- infrastructure/categorySvg.js
+++ infrastructure/categorySvg.js
@@ -1,8 +1,9 @@
 import { CategoryTypes } from "../entities/constants/categoryTypes.js";
+import { BASE_PATH } from "../entities/models/urlPaths.js";
 
 export const CategorySvg = {
-    [CategoryTypes.ProvincialCoatOfArms]: "/views/resources/images/svg/Shield.svg",
-    [CategoryTypes.Flowers]: "/views/resources/images/svg/Flower.svg",
-    [CategoryTypes.Fishes]: "/views/resources/images/svg/Fish.svg",
-    [CategoryTypes.ChecmicalElements]: "/views/resources/images/svg/ChemicalElement.svg",
+  [CategoryTypes.ProvincialCoatOfArms]: `${BASE_PATH}views/resources/images/svg/Shield.svg`,
+  [CategoryTypes.Flowers]: `${BASE_PATH}views/resources/images/svg/Flower.svg`,
+  [CategoryTypes.Fishes]: `${BASE_PATH}views/resources/images/svg/Fish.svg`,
+  [CategoryTypes.ChecmicalElements]: `${BASE_PATH}views/resources/images/svg/ChemicalElement.svg`,
 };
