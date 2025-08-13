5cbf5d1571cc05e1ea446eb4831c97264e7cffea
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-13 14:16:05 +0200

    Updated path to SVG-images from /resources/images/svg.

--- infrastructure/categorySvg.js
+++ infrastructure/categorySvg.js
@@ -2,8 +2,8 @@ import { CategoryTypes } from "../entities/constants/categoryTypes.js";
 import { BASE_PATH } from "../entities/models/urlPaths.js";
 
 export const CategorySvg = {
-  [CategoryTypes.ProvincialCoatOfArms]: `${BASE_PATH}views/resources/images/svg/Shield.svg`,
-  [CategoryTypes.Flowers]: `${BASE_PATH}views/resources/images/svg/Flower.svg`,
-  [CategoryTypes.Fishes]: `${BASE_PATH}views/resources/images/svg/Fish.svg`,
-  [CategoryTypes.ChecmicalElements]: `${BASE_PATH}views/resources/images/svg/ChemicalElement.svg`,
+  [CategoryTypes.ProvincialCoatOfArms]: `${BASE_PATH}resources/images/svg/Shield.svg`,
+  [CategoryTypes.Flowers]: `${BASE_PATH}resources/images/svg/Flower.svg`,
+  [CategoryTypes.Fishes]: `${BASE_PATH}resources/images/svg/Fish.svg`,
+  [CategoryTypes.ChecmicalElements]: `${BASE_PATH}resources/images/svg/ChemicalElement.svg`,
 };
