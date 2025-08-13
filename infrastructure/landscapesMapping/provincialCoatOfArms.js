9c284b98c8dc6f7c1ee395f72006d1b861218a91
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-13 13:42:23 +0200

    Changed absolute path to relative path for import and added BASE_PATH.

--- infrastructure/landscapesMapping/provincialCoatOfArms.js
+++ infrastructure/landscapesMapping/provincialCoatOfArms.js
@@ -1,35 +1,36 @@
 import { Landscapes } from "../../entities/constants/Landscapes.js";
 import { Landscape } from "../../entities/models/Landscape.js";
+import { BASE_PATH } from "../../entities/models/urlPaths.js";
 
 export const provincialCoatOfArms = Object.freeze([
     // Norrland
-    new Landscape(Landscapes.Lappland, "/resources/images/provincialCoatOfArms/Lappland.svg"),
-    new Landscape(Landscapes.Norrbotten, "/resources/images/provincialCoatOfArms/Norrbotten.svg"),
-    new Landscape(Landscapes.Västerbotten, "/resources/images/provincialCoatOfArms/Vasterbotten.svg"),
-    new Landscape(Landscapes.Jämtland, "/resources/images/provincialCoatOfArms/Jamtland.svg"),
-    new Landscape(Landscapes.Ångermanland, "/resources/images/provincialCoatOfArms/Angermanland.svg"),
-    new Landscape(Landscapes.Härjedalen, "/resources/images/provincialCoatOfArms/Harjedalen.svg"),
-    new Landscape(Landscapes.Medelpad, "/resources/images/provincialCoatOfArms/Medelpad.svg"),
-    new Landscape(Landscapes.Hälsingland, "/resources/images/provincialCoatOfArms/Halsingland.svg"),
-    new Landscape(Landscapes.Gästrikland, "/resources/images/provincialCoatOfArms/Gastrikland.svg"),
+    new Landscape(Landscapes.Lappland, `${BASE_PATH}resources/images/provincialCoatOfArms/Lappland.svg`),
+    new Landscape(Landscapes.Norrbotten, `${BASE_PATH}resources/images/provincialCoatOfArms/Norrbotten.svg`),
+    new Landscape(Landscapes.Västerbotten, `${BASE_PATH}resources/images/provincialCoatOfArms/Vasterbotten.svg`),
+    new Landscape(Landscapes.Jämtland, `${BASE_PATH}resources/images/provincialCoatOfArms/Jamtland.svg`),
+    new Landscape(Landscapes.Ångermanland, `${BASE_PATH}resources/images/provincialCoatOfArms/Angermanland.svg`),
+    new Landscape(Landscapes.Härjedalen, `${BASE_PATH}resources/images/provincialCoatOfArms/Harjedalen.svg`),
+    new Landscape(Landscapes.Medelpad, `${BASE_PATH}resources/images/provincialCoatOfArms/Medelpad.svg`),
+    new Landscape(Landscapes.Hälsingland, `${BASE_PATH}resources/images/provincialCoatOfArms/Halsingland.svg`),
+    new Landscape(Landscapes.Gästrikland, `${BASE_PATH}resources/images/provincialCoatOfArms/Gastrikland.svg`),
 
     // Svealand
-    new Landscape(Landscapes.Dalarna, "/resources/images/provincialCoatOfArms/Dalarna.svg"),
-    new Landscape(Landscapes.Värmland, "/resources/images/provincialCoatOfArms/Varmland.svg"),
-    new Landscape(Landscapes.Västmanland, "/resources/images/provincialCoatOfArms/Vastmanland.svg"),
-    new Landscape(Landscapes.Uppland, "/resources/images/provincialCoatOfArms/Uppland.svg"),
-    new Landscape(Landscapes.Närke, "/resources/images/provincialCoatOfArms/Narke.svg"),
-    new Landscape(Landscapes.Södermanland, "/resources/images/provincialCoatOfArms/Sodermanland.svg"),
+    new Landscape(Landscapes.Dalarna, `${BASE_PATH}resources/images/provincialCoatOfArms/Dalarna.svg`),
+    new Landscape(Landscapes.Värmland, `${BASE_PATH}resources/images/provincialCoatOfArms/Varmland.svg`),
+    new Landscape(Landscapes.Västmanland, `${BASE_PATH}resources/images/provincialCoatOfArms/Vastmanland.svg`),
+    new Landscape(Landscapes.Uppland, `${BASE_PATH}resources/images/provincialCoatOfArms/Uppland.svg`),
+    new Landscape(Landscapes.Närke, `${BASE_PATH}resources/images/provincialCoatOfArms/Narke.svg`),
+    new Landscape(Landscapes.Södermanland, `${BASE_PATH}resources/images/provincialCoatOfArms/Sodermanland.svg`),
 
     // Götaland
-    new Landscape(Landscapes.Dalsland, "/resources/images/provincialCoatOfArms/Dalsland.svg"),
-    new Landscape(Landscapes.Bohuslän, "/resources/images/provincialCoatOfArms/Bohuslan.svg"),
-    new Landscape(Landscapes.Västergötland, "/resources/images/provincialCoatOfArms/Vastergotland.svg"),
-    new Landscape(Landscapes.Östergötland, "/resources/images/provincialCoatOfArms/Ostergotland.svg"),
-    new Landscape(Landscapes.Halland, "/resources/images/provincialCoatOfArms/Halland.svg"),
-    new Landscape(Landscapes.Småland, "/resources/images/provincialCoatOfArms/Smaland.svg"),
-    new Landscape(Landscapes.Gotland, "/resources/images/provincialCoatOfArms/Gotland.svg"),
-    new Landscape(Landscapes.Öland, "/resources/images/provincialCoatOfArms/Oland.svg"),
-    new Landscape(Landscapes.Blekinge, "/resources/images/provincialCoatOfArms/Blekinge.svg"),
-    new Landscape(Landscapes.Skåne, "/resources/images/provincialCoatOfArms/Skane.svg")
+    new Landscape(Landscapes.Dalsland, `${BASE_PATH}resources/images/provincialCoatOfArms/Dalsland.svg`),
+    new Landscape(Landscapes.Bohuslän, `${BASE_PATH}resources/images/provincialCoatOfArms/Bohuslan.svg`),
+    new Landscape(Landscapes.Västergötland, `${BASE_PATH}resources/images/provincialCoatOfArms/Vastergotland.svg`),
+    new Landscape(Landscapes.Östergötland, `${BASE_PATH}resources/images/provincialCoatOfArms/Ostergotland.svg`),
+    new Landscape(Landscapes.Halland, `${BASE_PATH}resources/images/provincialCoatOfArms/Halland.svg`),
+    new Landscape(Landscapes.Småland, `${BASE_PATH}resources/images/provincialCoatOfArms/Smaland.svg`),
+    new Landscape(Landscapes.Gotland, `${BASE_PATH}resources/images/provincialCoatOfArms/Gotland.svg`),
+    new Landscape(Landscapes.Öland, `${BASE_PATH}resources/images/provincialCoatOfArms/Oland.svg`),
+    new Landscape(Landscapes.Blekinge, `${BASE_PATH}resources/images/provincialCoatOfArms/Blekinge.svg`),
+    new Landscape(Landscapes.Skåne, `${BASE_PATH}resources/images/provincialCoatOfArms/Skane.svg`)
 ]);
