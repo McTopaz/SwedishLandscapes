584a7b5277af01d58fb6a73e742a9b2564d9cf2e
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-14 14:58:06 +0200

    Added boiler plate for view.

---
+++ views/result/result.js
@@ -0,0 +1,13 @@
+import { Base } from "../base/base.js";
+import { game } from "../../entities/models/game.js";
+import { BASE_PATH } from "../../entities/models/urlPaths.js";
+
+export class Result extends Base {
+
+  init() {
+    super.init();
+  }
+
+  hanldeKeyboardEvent(event) {
+  }
+}
