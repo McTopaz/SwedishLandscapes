d9e87638bac134437e449540bf8abc387df0aa4f
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-08-15 16:23:35 +0200

    Added beta symbol to version.

--- entities/constants/game.js
+++ entities/constants/game.js
@@ -1,4 +1,4 @@
-export const Version = "1.0.0";
+export const Version = "1.0.0 β";
 export const Title = "Swedish Landscapes";
 export const SubTitle = "The landscapes of Sweden";
 export const CreatedBy = "Mc_Topaz";
